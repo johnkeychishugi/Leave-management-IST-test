1056a477d86a066312785413f80269ab8b935a33
John Chishugi <jkchishugi@gmail.com>
2025-04-21 14:48:41 +0200

    fx-done

--- frontend/app/dashboard/users/page.tsx
+++ frontend/app/dashboard/users/page.tsx
@@ -201,14 +201,14 @@ export default function UsersPage() {
             </p>
           </div>
           <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            <button
+            {/* <button
               type="button"
               onClick={() => openModal()}
               className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
             >
               <FiPlus className="mr-2" />
               Add User
-            </button>
+            </button> */}
           </div>
         </div>
         
